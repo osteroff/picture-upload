55f671df0db4435276fe1922f67416e920d8eaf0
Jeonghwan_Lee <leejeonghwan1324@gmail.com>
2018-03-17 00:04:37 +0900

    add login

--- #3 Dev/navigation_src/app.js
+++ #3 Dev/navigation_src/app.js
@@ -14,17 +14,24 @@ var port2 = 443;
 
 var app = express();
 
-var orientDB = require('orientjs');
-
-var server = orientDB({
-  host: 'localhost',
-  port: 2424,
-  username: 'root',
-  password: '1324803'
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1324803',
+  database : 'remember_travel_db'
 });
 
-var db = server.use('kumoh-guide');
-var sql = 'select from restaurant';
+// var orientDB = require('orientjs');
+//
+// var server = orientDB({
+//   host: 'localhost',
+//   port: 2424,
+//   username: 'root',
+//   password: '1324803'
+// });
+
+// var db = server.use('kumoh-guide');
 
 app.use(express.urlencoded());
 app.use(express.static('src/javascript'));
@@ -42,13 +49,53 @@ https.createServer(options, app).listen(port2, function(){
 });
 
 app.get('/', function (req, res) {
-  db.query(sql).then(function(restaurantList) {
-    console.log(restaurantList);
-    res.render('home', {restaurants: restaurantList});
-  })
+  // var sql = 'select from tour';
+  // db.query(sql).then(function(tourList) {
+  //   console.log(tourList);
+  //   res.render('home', {tourList: tourList});
+  // })
+
+  // var sql = 'select * from tour';
+  // conn.query(sql, function (error, tourList, fields) {
+  //   if (error) {
+  //     console.log(err);
+  //   }
+  //   else {
+  //     res.render('home', {tourList: tourList});
+  //   }
+  // });
+  console.log('location!');
+  res.render('location');
   // res.render('home');
 });
 
+app.get('/location', function(req, res) {
+  var longitude = req.query.longitude;
+  var latitude = req.query.latitude;
+
+  var sql = 'select * from tour';
+  conn.query(sql, function (error, tourList, fields) {
+    if (error) {
+      console.log(err);
+    }
+    else {
+      tourList.sort(function(a, b) {
+        // console.log('run');
+        var long = parseFloat(longitude);
+        var lat = parseFloat(latitude);
+        var distanceA = (long - a.longitude) * (long - a.longitude) + (lat - a.latitude) * (lat - a.latitude);
+        var distanceB = (longitude - b.longitude) * (longitude - b.longitude) + (lat - b.latitude) * (lat - b.latitude);
+        console.log(distanceA, distanceB);
+        console.log(longitude, latitude);
+        console.log(a.longitude, a.latitude);
+        console.log(b.longitude, b.latitude)
+        // return distanceA - distanceB;
+        return distanceA - distanceB;
+      });
+      res.render('home', {tourList: tourList, longitude: longitude, latitude: latitude});
+    }
+  });
+});
 app.get('/work', function(req, res) {
   console.log("Working!");
   var startLongitude = req.query.startLongitude;
